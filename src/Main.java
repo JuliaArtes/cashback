a508cd78e2db2cfd78d3c80f890ad99a5a526cf1
Student <student@itpark.ru>
2019-09-14 15:04:48 +0300

    logik without limit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int amountInRegular = 100;
+        int amountInHigh = 1000;
+        int amointInSpecial = 10000;
 
+        int percentIRegular = 1;
+        int percentInHigh = 5;
+        int percentInSpecial = 30;
+
+        int result = (amountInRegular * percentIRegular + amountInHigh * percentInHigh + amointInSpecial * percentInSpecial) / 100;
+        System.out.println(result);
     }
 }
